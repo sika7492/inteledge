c683da558e03a27906b82eb8ee75a12fe7c1c9bd
sika7492 <154478957+sika7492@users.noreply.github.com>
2024-01-30 12:18:20 +0900

    Add files via upload

---
+++ RaspberryPi/240130/led_rgb.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <wiringPi.h>
+#include <unistd.h>
+#include <time.h>
+
+#define LED_RED 7
+#define LED_GREEN 21
+#define LED_BLUE 22
+
+int main(void){
+	
+	if(wiringPiSetup() ==-1)
+			return 1;
+	pinMode(LED_RED,OUTPUT);
+	pinMode(LED_GREEN,OUTPUT);
+	pinMode(LED_BLUE,OUTPUT);
+	
+digitalWrite(LED_RED,0);
+		digitalWrite(LED_GREEN,0);
+		digitalWrite(LED_BLUE,0);
+		printf("3 Color LED Control Start !! \n");
+		
+		for(int i=0;i<20;i++){
+			printf("RED LED on!!\n ");
+			digitalWrite(LED_RED,1);
+			usleep(500000);
+		printf("Red LED off  !! \n Green LED on !! \n");
+			digitalWrite(LED_RED,0);
+			digitalWrite(LED_GREEN,1);
+			usleep(500000);
+printf("Green LED off!!\n BLUE LED on !!\n");
+			digitalWrite(LED_GREEN,0);
+			digitalWrite(LED_BLUE,1);
+
+			usleep(500000);
+		printf("Blue led off!!\n");
+				digitalWrite(LED_BLUE,0);
+		}
+		return 0;
+}
+		
+			
