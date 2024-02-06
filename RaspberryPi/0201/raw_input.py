1467f3f2d202587bb637f44c4e6e69822aae7237
sika7492 <154478957+sika7492@users.noreply.github.com>
2024-02-06 11:56:42 +0900

    Delete RaspberryPi/0201 directory

--- RaspberryPi/0201/raw_input.py
+++
@@ -1,6 +0,0 @@
-try:
-    while True:
-        userinput= input() #for string
-        print(userinput)
-except KeyboardInterrupt:
-    pass
--- RaspberryPi/0201/readme.md
+++
@@ -1,2 +0,0 @@
-## 라즈베리파이 PIR,BUTTON,SOUND_SENSOR 사용방법과 예제
-
