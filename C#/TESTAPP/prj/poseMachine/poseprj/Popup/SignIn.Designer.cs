515e11ff4a4c514b7784fd6d29069dcb66c536d0
sika7492 <154478957+sika7492@users.noreply.github.com>
2024-04-12 16:30:29 +0900

    Add files via upload

---
+++ C#/TESTAPP/prj/poseMachine/poseprj/Popup/SignIn.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace poseprj.Popup
+{
+    partial class SignIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SignIn));
+            pb = new PictureBox();
+            textBox1 = new TextBox();
+            txtuser = new TextBox();
+            txtpwd = new TextBox();
+            ckbsave = new CheckBox();
+            bnlogin = new Button();
+            bncancel = new Button();
+            txdate = new TextBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)pb).BeginInit();
+            SuspendLayout();
+            // 
+            // pb
+            // 
+            pb.Image = Properties.Resources.make;
+            pb.InitialImage = (Image)resources.GetObject("pb.InitialImage");
+            pb.Location = new Point(16, 29);
+            pb.Name = "pb";
+            pb.Size = new Size(48, 48);
+            pb.SizeMode = PictureBoxSizeMode.StretchImage;
+            pb.TabIndex = 0;
+            pb.TabStop = false;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.Control;
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("맑은 고딕", 27.75F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            textBox1.Location = new Point(70, 29);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(291, 55);
+            textBox1.TabIndex = 900;
+            textBox1.Text = "테스트 프로젝트";
+            // 
+            // txtuser
+            // 
+            txtuser.Location = new Point(16, 140);
+            txtuser.Name = "txtuser";
+            txtuser.PlaceholderText = "사용자 아이디를 입력하세요.";
+            txtuser.Size = new Size(242, 26);
+            txtuser.TabIndex = 4;
+            txtuser.KeyDown += txtpwd_KeyDown;
+            // 
+            // txtpwd
+            // 
+            txtpwd.Location = new Point(16, 187);
+            txtpwd.Name = "txtpwd";
+            txtpwd.PasswordChar = '*';
+            txtpwd.PlaceholderText = "비밀번호를 입력하세요.";
+            txtpwd.Size = new Size(306, 26);
+            txtpwd.TabIndex = 5;
+            txtpwd.KeyDown += txtpwd_KeyDown;
+            // 
+            // ckbsave
+            // 
+            ckbsave.AutoSize = true;
+            ckbsave.Location = new Point(264, 140);
+            ckbsave.Name = "ckbsave";
+            ckbsave.Size = new Size(58, 20);
+            ckbsave.TabIndex = 6;
+            ckbsave.Text = "저장";
+            ckbsave.UseVisualStyleBackColor = true;
+            ckbsave.CheckedChanged += ckbsave_CheckedChanged;
+            // 
+            // bnlogin
+            // 
+            bnlogin.Location = new Point(16, 420);
+            bnlogin.Name = "bnlogin";
+            bnlogin.Size = new Size(133, 86);
+            bnlogin.TabIndex = 7;
+            bnlogin.Text = "로그인";
+            bnlogin.UseVisualStyleBackColor = true;
+            bnlogin.Click += bnlogin_Click;
+            // 
+            // bncancel
+            // 
+            bncancel.Location = new Point(189, 469);
+            bncancel.Name = "bncancel";
+            bncancel.Size = new Size(133, 37);
+            bncancel.TabIndex = 10;
+            bncancel.Text = "CANCEL";
+            bncancel.UseVisualStyleBackColor = true;
+            // 
+            // txdate
+            // 
+            txdate.BackColor = SystemColors.Control;
+            txdate.BorderStyle = BorderStyle.None;
+            txdate.Font = new Font("돋움", 28F);
+            txdate.ForeColor = Color.Maroon;
+            txdate.Location = new Point(16, 243);
+            txdate.Multiline = true;
+            txdate.Name = "txdate";
+            txdate.Size = new Size(313, 118);
+            txdate.TabIndex = 910;
+            txdate.Text = "2024년 4월 12일 \r\n\r\n11시 10분 52초\r\n";
+            txdate.TextAlign = HorizontalAlignment.Center;
+            // 
+            // timer1
+            // 
+            timer1.Interval = 1000;
+            timer1.Tick += timer1_Tick;
+            // 
+            // SignIn
+            // 
+            AutoScaleDimensions = new SizeF(9F, 16F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(384, 561);
+            Controls.Add(txdate);
+            Controls.Add(bncancel);
+            Controls.Add(bnlogin);
+            Controls.Add(ckbsave);
+            Controls.Add(txtpwd);
+            Controls.Add(txtuser);
+            Controls.Add(textBox1);
+            Controls.Add(pb);
+            Font = new Font("돋움", 12F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(4, 3, 4, 3);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "SignIn";
+            Text = "SignIn";
+            Load += SignIn_Load;
+            ((System.ComponentModel.ISupportInitialize)pb).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pb;
+        private TextBox textBox1;
+        private TextBox txtuser;
+        private TextBox txtpwd;
+        private CheckBox ckbsave;
+        private Button bnlogin;
+        private Button bncancel;
+        private TextBox txdate;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ C#/TESTAPP/prj/poseMachine/poseprj/Popup/SignIn.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+using System.Xml;
+using System.Xml.Linq;
+using MySqlConnector;
+
+namespace poseprj.Popup
+{
+    public partial class SignIn : Form
+    {
+        private string svr_IP;
+        private string DBname;
+        private string DBport;
+        private string XmlUser;
+        private string Loc;
+
+        private string ConString;           // 현재 string 값
+        private string LocalConstring;      //
+        private string remoteConstring;
+
+
+
+        public SignIn()
+        {
+            InitializeComponent();
+        }
+
+        private void SignIn_Load(object sender, EventArgs e)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(@".\Sys.Config");
+            XmlElement root = xmlDoc.DocumentElement;
+
+            // 노드요소들
+            XmlNodeList nodes = root.ChildNodes;
+
+            foreach (XmlNode node in nodes)
+            {
+                switch (node.Name)
+                {
+                    case "Loc":
+                        Loc = node.InnerText;
+                        break;
+                    case "Remote":
+                        svr_IP = node.InnerText;
+                        break;
+                    case "DBname":
+                        DBname = node.InnerText;
+                        break;
+                    case "DBport":
+                        DBport = node.InnerText;
+                        break;
+                    case "User":
+                        XmlUser = node.InnerText;
+                        break;
+                }
+
+            }
+
+            LocalConstring = $@" Server=localhost;Port={DBport};DATABASE={DBname};UID=iphak;PASSWORD=usr123; ";
+
+            // MessageBox.Show(LocalConstring);
+
+            remoteConstring = $@" Server={svr_IP};Port={DBport};DATABASE={DBname};UID=iphak;PASSWORD=usr123; ";
+
+            // MessageBox.Show(remoteConstring);
+
+            ConString = (Loc == "LOCAL") ? LocalConstring : remoteConstring;
+
+            // mxl text 용 코드
+            // MessageBox.Show("[" + svr_IP + ":" + DBname + ":" + DBport + ";" + User + "]");
+
+            if (XmlUser == "")
+            {
+                ckbsave.Checked = false;
+                txtuser.Enabled = true;
+
+                txtuser.Text = "";
+                txtuser.Focus();
+            }
+            else
+            {
+                ckbsave.Checked = true;
+                txtuser.Enabled = false;
+
+                txtuser.Text = XmlUser;
+                txtpwd.Focus();
+            }
+
+            timer1.Enabled = true;
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            CultureInfo cultures = CultureInfo.CreateSpecificCulture("ko-KR");
+
+            string timeStr = DateTime.Now.ToString(string.Format("yyyy년 M월 dd일 (ddd)\r\ntt hh:mm:sss", cultures));
+
+            txdate.Text = timeStr;
+
+        }
+
+        private void ckbsave_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ckbsave.Checked == false)
+            {
+                txtuser.Text = "";
+                txtuser.Enabled = true;
+
+                txtuser.Focus();
+
+            }
+            else
+            {
+
+                XmlDocument sysConfig = new XmlDocument();
+                sysConfig.Load(@".\Sys.Config");
+
+                XmlNode nodeUser = sysConfig.DocumentElement.SelectSingleNode("/DBinfs/User");
+
+                nodeUser.InnerText = txtuser.Text.Replace("\r\n", "");
+
+                sysConfig.Save(@".\Sys.Config");
+
+                txtuser.Enabled = false;
+                txtpwd.Focus();
+            }
+
+        }
+
+        private void txtuser_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txtpwd.Focus();
+            }
+        }
+
+        private void txtpwd_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+
+                bnlogin.Focus();
+
+
+
+            }
+
+        }
+
+        private void bnlogin_Click(object sender, EventArgs e)
+        {
+            if (txtuser.Text == "" || txtpwd.Text == "") 
+            {
+                MessageBox.Show("사용자 id 혹은 비밀번호를 입력하세요");
+                return;
+            }
+            MySqlConnection con = new MySqlConnection(ConString);
+            string sSQL = $@"SELECT usrid, usralias, kind, auth F
+                             FROM usr
+                             WHERE usrid = '{txtuser.Text}'
+                             AND usrpwd = password('{txtpwd.Text}') ";
+
+            try
+            {
+                con.Open();
+                MySqlCommand cmd = new MySqlCommand(sSQL, con);
+
+                var Read = cmd.ExecuteReader();
+
+                if (Read.Read())
+                {
+                    MessageBox.Show("DB 진입이 인정되었습니다.");
+                }
+                else
+                {
+                    MessageBox.Show("사용자 승인여부를 시스템 관리자에게 문의하세요.");
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
